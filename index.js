94a686573a07ed7fd042556fc401bc6cbf491cc4
hiroshi noguchi <hiroshi.noguchi@leonisand.co>
2016-10-28 23:14:44 +0900

    update index.js
    add binder
    
    add resource manager

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 const _ = require('lodash');
 const path = require('path');
+const binder = require('./lib/binder');
+const ResourceManager = require('./lib/resource_manager');
 
 class Internationalize {
 
@@ -11,23 +13,65 @@ class Internationalize {
       directory: path.join(__dirname, '/locales'),
       extension: 'json'
     };
-    this._locale = null;
+    this._resource = new ResourceManager();
+    this._current = null;
   }
 
   configure(options) {
     _.extend(this._options, options);
+    if (_.isEmpty(this._current)) {
+      this._current = this._options.defaultLocale;
+    }
+  }
+
+  init(request, response, next) {
+    if (!_.isObject(request)) {
+      throw new Error('must be called with one parameter minimum)');
+    }
+    this._apply(request);
+    this.setLocale(this._current);
+
+    if (_.isObject(response)) {
+      this._apply(response);
+      this.setLocale(this._current);
+    }
+
+    if (_.isFunction(next)) {
+      return next();
+    }
+  }
+
+  getLocale() {
+    return this._current || this._options.defaultLocale;
   }
 
   setLocale(locale) {
     if (!_.isString(locale)) {
       throw new Error('invalid variable type, allowed only a string.');
     }
-    this._locale = locale;
+    if (this._current === locale) {
+      return;
+    }
+    const opt = this._options;
+    this._resource.set({
+      locale: locale,
+      directory: opt.directory,
+      extension: opt.extension
+    });
   }
 
-  getLocale() {
-    return this._locale || this._options.defaultLocale;
+  _apply(object) {
+    if (binder.exec(object, this)) {
+      return;
+    }
+    const self = this;
+    ['res', 'object'].forEach((key) => {
+      if (object[key]) {
+        self._apply(object);
+      }
+    });
   }
+
 }
 
 module.exports = new Internationalize();
